715af9537898bdf03ee590f4d7e867bb23fdbc7d
Jovana Veljancic <veljancic.jovana@gmail.com>
2016-11-07 15:14:06 +0100

    Submit the form
    Create list item for every input and add that input to the list. Submit and reset the form.

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+var form = document.getElementById("todo-form");
+
+function getInput(event) {
+    event.preventDefault();
+    var input = document.getElementById("todo").value;
+    if (input == "" || input == null) {
+        return;
+    }
+    form.reset();
+
+    // Create a new list item
+    var litem = document.createElement('li');
+    litem.innerHTML = input;
+
+    // Create a "close" button and append it to each list item
+    var close = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    close.className = "close";
+    close.appendChild(txt);
+    litem.appendChild(close);
+
+    //Click on a "close" button to remove list item
+    close.onclick = function () {
+        this.parentNode.remove();
+    };
+
+    // Add list item to the list
+    document.getElementById('update').appendChild(litem);
+}
+
+form.addEventListener("submit", getInput, false);
