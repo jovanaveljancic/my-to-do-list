85a02be33316b2746f326db088ede2651cc1f718
Jovana Veljancic <veljancic.jovana@gmail.com>
2016-11-08 14:56:51 +0100

    Add a "checked" symbol when clicking on a list item

--- script.js
+++ script.js
@@ -28,5 +28,12 @@ function getInput(event) {
     // Add list item to the list
     document.getElementById('update').appendChild(litem);
 }
+    form.addEventListener("submit", getInput, false);
 
-form.addEventListener("submit", getInput, false);
+    // Add a "checked" symbol when clicking on a list item
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
